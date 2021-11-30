cb0b351951ffb21f853e4dedcf73e5936c2ce94b
MinSang Yu <nbghza@gmail.com>
2021-11-30 15:39:23 +0900

    Initial commit

---
+++ src/test/java/com/example/amare/AmareApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.amare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmareApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
