8cff4d0b629df1a126a178c734f6b091c1a01e1f
MinSang Yu <nbghza@gmail.com>
2021-11-30 16:21:19 +0900

    gradle, 디렉터리, dao 세팅완료

---
+++ src/main/java/com/example/amare/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.example.amare.dao;
+
+import com.example.amare.dto.User;
+
+import java.util.List;
+
+public interface UserDao
+{
+    void insertUser(User user);
+    List<User> selectUsers();
+    User findUserById(String id);
+
+}
---
+++ src/main/java/com/example/amare/dto/User.java
@@ -0,0 +1,19 @@
+package com.example.amare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User
+{
+    String user_id;
+    String user_password;
+    String user_nickname;
+    int user_age;
+    String user_address;
+    String user_sex;
+    String user_height;
+    String user_tag;
+
+}
